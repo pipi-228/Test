045054e3703736380c8bd8636f74fcfd70fd67eb
alabaster246 <svlasov008@gmail.com>
2025-04-23 20:52:16 +0300

    upd

--- myproject/mysite/volunteers/admin.py
+++ myproject/mysite/volunteers/admin.py
@@ -3,11 +3,20 @@ from .models import Volunteer
 
 @admin.register(Volunteer)
 class VolunteerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'inn', 'rank', 'bonus_points', 'get_rank_group_display')
-    list_filter = ('rank',)  # Фильтруем по реальному полю rank
+    list_display = ('rank', 'full_name', 'inn', 'birth_date', 'achievements_short', 'bonus_points')
+    list_display_links = ('rank', 'full_name')
+    list_filter = ('rank',)
     search_fields = ('full_name', 'inn', 'email')
-    ordering = ('rank',)
+    list_per_page = 200  # Увеличиваем количество записей на странице
+    ordering = ('rank',)  # Сортировка по рангу по умолчанию
 
-    def get_rank_group_display(self, obj):
-        return obj.rank_group
-    get_rank_group_display.short_description = 'Группа'
+    def achievements_short(self, obj):
+        return obj.achievements[:50] + "..." if len(obj.achievements) > 50 else obj.achievements
+    achievements_short.short_description = 'Достижения'
+    
+    # Добавляем action для админки
+    actions = ['reset_bonus_points']
+    
+    def reset_bonus_points(self, request, queryset):
+        queryset.update(bonus_points=0)
+    reset_bonus_points.short_description = "Обнулить бонусные баллы"
