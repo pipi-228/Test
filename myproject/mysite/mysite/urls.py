e5418ad5ed76d7d9464cce22f8b286653dd4d169
alabaster246 <svlasov008@gmail.com>
2025-04-23 22:36:36 +0300

    upd2

--- myproject/mysite/mysite/urls.py
+++ myproject/mysite/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from volunteers import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', views.leaderboard, name='leaderboard'),
+    #path('profile/', views.profile, name='profile'),
+    #path('import/', views.import_data, name='import'),
 ]
--- myproject/mysite/volunteers_portal/settings.py
+++
@@ -1,9 +0,0 @@
-INSTALLED_APPS = [
-    ...
-    'volunteers',
-]
-
-AUTH_USER_MODEL = 'auth.User'
-
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/profile/'
