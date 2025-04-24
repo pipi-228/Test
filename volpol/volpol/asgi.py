9d4da2ac5b7813d23349a190e9018abf54aa04f3
Sertiz <cuprinastas5@gmail.com>
2025-04-24 09:34:39 +0300

    rearmed again

--- volpol/volpol/asgi.py
+++ volpol/volpol/asgi.py
@@ -1,28 +1,16 @@
 """
-<<<<<<<< HEAD:volpol/volunteer_platform/asgi.py
-ASGI config for volunteer_platform project.
-========
 ASGI config for volpol project.
->>>>>>>> origin/volpol:volpol/volpol/asgi.py
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<<< HEAD:volpol/volunteer_platform/asgi.py
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-========
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
->>>>>>>> origin/volpol:volpol/volpol/asgi.py
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-<<<<<<<< HEAD:volpol/volunteer_platform/asgi.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer_platform.settings')
-========
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volpol.settings')
->>>>>>>> origin/volpol:volpol/volpol/asgi.py
 
 application = get_asgi_application()
--- volpol/volpol/wsgi.py
+++ volpol/volpol/wsgi.py
@@ -1,28 +1,16 @@
 """
-<<<<<<<< HEAD:volpol/volunteer_platform/wsgi.py
-WSGI config for volunteer_platform project.
-========
 WSGI config for volpol project.
->>>>>>>> origin/volpol:volpol/volpol/wsgi.py
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<<< HEAD:volpol/volunteer_platform/wsgi.py
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-========
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
->>>>>>>> origin/volpol:volpol/volpol/wsgi.py
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-<<<<<<<< HEAD:volpol/volunteer_platform/wsgi.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volunteer_platform.settings')
-========
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volpol.settings')
->>>>>>>> origin/volpol:volpol/volpol/wsgi.py
 
 application = get_wsgi_application()
